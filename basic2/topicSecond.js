962a941ea018e1cce116bd154f7b857c57a88fe1
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-11-21 15:50:38 -0300

    Add files via upload

--- basic2/topicSecond.js
+++ basic2/topicSecond.js
@@ -1,15 +1,14 @@
 function myFunctionTwo() {
   idArray = new Array()
-  idArray [1] = "<b>TOPIC 2:</b> Which was <u>your favorite food when you were a kid</u>? Talk about your favorite snacks when you were a kid / in school."
-  idArray [2] = "<b>TOPIC 2:</b> What was <u>the last movie you saw</u>? Talk about what the main story was, who were the main characters, what happened to them."
-  idArray [3] = "<b>TOPIC 2:</b> Talk about the <u>last time you went out</u> with a friend or family member. You can say <u>where you went, what you did</u> and why."
-  idArray [4] = "<b>TOPIC 2:</b> Talk about the <u>last time you went on holiday</u>. You can say <u>who you went with, where you went, what you did</u> and if you enjoyed it."
-  idArray [5] = "<b>TOPIC 2:</b> Talk about <u>your last trip on summer vacation, including the activities you did</u>, including the people you were with, the food you ate, etc."
-  idArray [6] = "<b>TOPIC 2:</b> Talk about <u>the last time you went camping, went on a road trip or stayed at a friend's house</u>, including how long it was, where you stayed, what you did or ate, etc."
-  idArray [7] = "<b>TOPIC 2:</b> Talk about <u>the last time you were outside in nature</u> (like the countryside, a forest, river or lake). You can mention was there, the activities you did, the people you met, etc."
+  idArray [1] = "<b>TOPIC 2:</b> What was <u>the last movie you saw</u>? Talk about what the main story was, who were the main characters, what happened to them. (Units 7 & 8A: Past Simple & Irregular verbs)."
+  idArray [2] = "<b>TOPIC 2:</b> Talk about the <u>last time you went out</u> with a friend or family member. You can say <u>where you went, what you did</u> and why. (Units 7 & 8A: Past Simple & Irregular verbs)."
+  idArray [3] = "<b>TOPIC 2:</b> Talk about the <u>last time you went on holiday</u>. You can say <u>who you went with, where you went, what you did</u> and if you enjoyed it. (Units 7 & 8A: Past Simple & Irregular verbs)."
+  idArray [4] = "<b>TOPIC 2:</b> Talk about <u>your last trip on summer vacation, including the activities you did</u>, including the people you were with, the food you ate, etc. (Units 7 & 8A: Past Simple & Irregular verbs)."
+  idArray [5] = "<b>TOPIC 2:</b> Talk about <u>the last time you went camping, went on a road trip or stayed at a friend's house</u>, including how long it was, where you stayed, what you did or ate, etc. (Units 7 & 8A: Past Simple & Irregular verbs)."
+  idArray [6] = "<b>TOPIC 2:</b> Talk about <u>the last time you were outside in nature</u> (like the countryside, a forest, river or lake). You can mention was there, the activities you did, the people you met, etc. (Units 7 & 8A: Past Simple & Irregular verbs)."
 
   document.getElementById("select2").onclick = myFunctionTwo;
-  randomParagraph = Math.floor(Math.random()*7);
+  randomParagraph = Math.floor(Math.random()*6);
 
   document.getElementById("result-two").innerHTML = idArray[randomParagraph + 1];       
 }
