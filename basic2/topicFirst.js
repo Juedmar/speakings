7545cfb5588dfb00cd9ff97716aeed2f6658360d
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-12-02 16:09:57 -0300

    Add files via upload

--- basic2/topicFirst.js
+++ basic2/topicFirst.js
@@ -6,7 +6,7 @@ function myFunctionOne() {
   idArray [4] = "<b>TOPIC 1:</b> <u>Talk about your pastimes & hobbies</u>. Mention what activities you like doing, when you do them, how frequent, why you like them, etc. Do you do them by yourself, or with your friends? (Units 5C & 6B)."
   idArray [5] = "<b>TOPIC 1:</b> <u>Talk about all the vegetables you like or dislike</u>, including their names, why you like or dislike them, how often you eat them, etc. How do they taste? In what format or containers do they come? (Unit 5C: Present Simple / 6B: Prefence verbs)."
   idArray [6] = "<b>TOPIC 1:</b> Imagine you are in the middle of a busy city area. <u>Describe the things that are happening & mention all there is around you</u> at that moment. You can use your imagination. (Unit 5C: Present Continuous)."
-  idArray [7] = "<b>TOPIC 1:</b> Looks outside your window / outside your front door. <u>Describe all the things that are happening at that moment, including all there is around you</u>. (Unit 5C: Present Continuous)."
+  idArray [7] = "<b>TOPIC 1:</b> Look outside your window / outside your front door. <u>Describe all the things that are happening at that moment, including all there is around you</u>. (Unit 5C: Present Continuous)."
 
   document.getElementById("select1").onclick = myFunctionOne;
   randomParagraph = Math.floor(Math.random()*7);
